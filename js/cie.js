defa0d68d548e405f3a904d367b80b205def5262
bryan@thrive-studios.com <bryan@thrive-studios.com>
2013-06-03 14:10:08 -0400

    latest files

--- js/cie.js
+++ js/cie.js
@@ -1,3 +1,54 @@
 $(document).ready(function(e) {
-	$('.factile').factile();
+	$('.factile.mini').factile({
+		mini: true,
+		dragdrop: true
+	});
+	
+	$('.factile.full').factile({
+		mini: false,
+		dragdrop: false
+	});
+	
+	$('#selected-factiles').droppable({
+		//activeClass: "ui-state-hover",
+		//hoverClass: "ui-state-active",
+		hoverClass: "container-hover",
+		drop: function(event, ui) {
+			//$(this).addClass("ui-state-highlight");
+			$(this).append(ui.draggable);
+			$(this).find('.placeholder').before(ui.draggable);
+			ui.draggable.css({
+				left: 'auto',
+				top: 'auto'
+			}).find('.mini-expand').hide();
+		}
+	});
+	
+	//$('#available-factiles').droppable({
+	$('#available-factiles').droppable({
+		//activeClass: "ui-state-hover",
+		hoverClass: "container-hover",
+		drop: function(event, ui) {
+			//$(this).addClass("ui-state-highlight");
+			$(this).append(ui.draggable);
+			$(this).find('.placeholder').before(ui.draggable);
+			ui.draggable.css({
+				left: 'auto',
+				top: 'auto'
+			}).find('.mini-expand').show();
+		}
+	});
+	
+	$(".scroll-wrapper").mCustomScrollbar({
+		horizontalScroll: true,
+		autoDraggerLength: true,
+		scrollButtons:{
+			enable: true,
+			scrollType: 'continuous'			
+		},
+		advanced:{
+			autoExpandHorizontalScroll: true,
+			updateOnContentResize: true
+		}
+	});
 });
--- js/factile.js
+++ js/factile.js
@@ -3,6 +3,8 @@
 	$.fn.factile = function(options){
 	
 		var settings = $.extend({
+			mini:		false,
+			dragdrop:	false,
 			view: 		true,
 			fav: 		true,
 			share: 		true,
@@ -19,14 +21,69 @@
 			if(settings.add) actionBar.append('<a href="#" class="btn no-text"><span class="icon icon-add"></span>Add</a>');
 
 			$(this).append(actionBar);
-			$(this).hover(
-				function(){
-					$(this).children('.factile-actions').slideDown('fast');
-				},
-				function(){
-					$(this).children('.factile-actions').slideUp('fast');
-				}
-			)
+			
+			// add expand/collapse capability
+			if(settings.mini){
+				var expandIcon = $('<div />').addClass('mini-expand').html('<span class="icon icon-expand"></span>');
+				
+				expandIcon.click(function(){
+					var that = $(this).parent();
+					console.log(that);
+					if(that.hasClass('mini')){
+						that.draggable('disable');
+						that.find('.factile-header h1').fadeOut(100, function(){
+							that.animate({
+								height: 266,
+								width:195,
+								margin:0,
+								paddingRight:6
+							}, 100,	function(){
+								that.find('.factile-full').fadeIn(100, function(){
+									that.removeClass('mini');
+									that.find('.factile-header h1').fadeIn();
+								});
+							});
+						});
+						that.find('.icon-expand').removeClass('icon-expand').addClass('icon-collapse');
+					} else {
+						that.draggable( "enable" );
+						that.find('.factile-header h1').fadeOut(100, function(){
+							that.find('.factile-full').fadeOut(100, function(){
+								that.animate({
+									height: 65,
+									width:74,
+									margin:4,
+									paddingRight:0
+								}, 100,	function(){
+									that.addClass('mini');
+									that.find('.factile-header h1').fadeIn();
+								});
+							});
+						});
+						that.find('.icon-collapse').removeClass('icon-collapse').addClass('icon-expand');
+					}
+				});
+				
+				$(this).append(expandIcon);
+			} else {
+				$(this).hover(
+					function(){
+						$(this).children('.factile-actions').slideDown('fast');
+					},
+					function(){
+						$(this).children('.factile-actions').slideUp('fast');
+					}
+				)
+			}
+			
+			if(settings.dragdrop){
+				$(this).draggable({
+					appendTo: "body",
+					helper: "clone",
+					revert: "invalid",
+					zIndex:999
+				});
+			}
 			
 			if($.isFunction(settings.complete)){
 				settings.complete.call(this);
